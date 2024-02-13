918dfba82b5885b68c5732272e1b9ecaa828f7b3
Kyle Reynolds <kyle.reynolds@bridgerphotonics.com>
2024-02-13 12:05:18 -0700

    small tweaks

--- frontend_react/src/components/Navbar.js
+++ frontend_react/src/components/Navbar.js
@@ -84,156 +84,8 @@ function Navbar(props) {
           </Link>
           <div className="flex items-center ml-auto space-x-1 lg:space-x-4">
             <ul className="hidden lg:flex items-center ml-auto">
-              <li className="relative group">
-                <span className={`${classes.navLink}`} href="">
-                  <span>Solutions</span>
-                  <ChevronDownIcon className={classes.navLinkIcon} />
-                </span>
-                <div
-                  className={`${classes.dropdown.base} ${classes.dropdown.large} ${classes.dropdown.center}`}
-                >
-                  <div className={`${classes.dropdown.inner}`}>
-                    <div className="p-3">
-                      <div className="px-3 pt-3">
-                        <h4 className={`${classes.dropdown.title}`}>
-                          Featured Products
-                        </h4>
-                      </div>
-                      <nav className="flex flex-col space-y-1">
-                        <Link
-                          to="/"
-                          className={`hover:text-blue-600 hover:bg-blue-50 ${classes.dropdown.feature}`}
-                        >
-                          <div>
-                            <FeatureIcon color="blue">
-                              <CodeBracketIcon />
-                            </FeatureIcon>
-                          </div>
-                          <div>
-                            <h5 className={`${classes.dropdown.featureName}`}>
-                              HTML Editor
-                            </h5>
-                            <p
-                              className={`${classes.dropdown.featureDescription}`}
-                            >
-                              Write and manipulate your markup directly in your
-                              browser
-                            </p>
-                          </div>
-                        </Link>
-                        <Link
-                          to="/"
-                          className={`hover:text-emerald-600 hover:bg-emerald-50 ${classes.dropdown.feature}`}
-                        >
-                          <div>
-                            <FeatureIcon color="emerald">
-                              <SwatchIcon />
-                            </FeatureIcon>
-                          </div>
-                          <div>
-                            <h5 className={`${classes.dropdown.featureName}`}>
-                              CSS Editor
-                            </h5>
-                            <p
-                              className={`${classes.dropdown.featureDescription}`}
-                            >
-                              Style your markup code with smart tools supporting
-                              Sass
-                            </p>
-                          </div>
-                        </Link>
-                        <Link
-                          to="/"
-                          className={`hover:text-orange-600 hover:bg-orange-50 ${classes.dropdown.feature}`}
-                        >
-                          <div>
-                            <FeatureIcon color="orange">
-                              <PuzzlePieceIcon />
-                            </FeatureIcon>
-                          </div>
-                          <div>
-                            <h5 className={`${classes.dropdown.featureName}`}>
-                              Web Page Builder
-                            </h5>
-                            <p
-                              className={`${classes.dropdown.featureDescription}`}
-                            >
-                              Explore all available components and build your
-                              website
-                            </p>
-                          </div>
-                        </Link>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="relative group">
-                <span className={`${classes.navLink}`} href="">
-                  <span>Resources</span>
-                  <ChevronDownIcon className={classes.navLinkIcon} />
-                </span>
-                <div
-                  className={`${classes.dropdown.base} ${classes.dropdown.large} ${classes.dropdown.center}`}
-                >
-                  <div className={`${classes.dropdown.inner}`}>
-                    <div className="grid grid-cols-2">
-                      <div className="p-6 space-y-6">
-                        <h4 className={`${classes.dropdown.title}`}>Learn</h4>
-                        <nav className="flex flex-col space-y-3">
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            Resource Center
-                          </Link>
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            Developer
-                          </Link>
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            User Guides
-                          </Link>
-                        </nav>
-                      </div>
-                      <div className="p-6 space-y-6">
-                        <h4 className={`${classes.dropdown.title}`}>Links</h4>
-                        <nav className="flex flex-col space-y-3">
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            Starters
-                          </Link>
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            Video Library
-                          </Link>
-                          <Link to="/" className={`${classes.dropdown.link}`}>
-                            Tutorials
-                          </Link>
-                        </nav>
-                      </div>
-                    </div>
-                    <div className="grid grid-cols-2 bg-gray-50">
-                      <nav className="p-6 flex flex-col space-y-4">
-                        <Link to="/" className={`${classes.dropdown.link}`}>
-                          <BeakerIcon className={classes.dropdown.icon} />
-                          <span>API Reference</span>
-                        </Link>
-                        <Link to="/" className={`${classes.dropdown.link}`}>
-                          <DocumentIcon className={classes.dropdown.icon} />
-                          <span>Documentation</span>
-                        </Link>
-                      </nav>
-                      <nav className="p-6 flex flex-col space-y-4">
-                        <Link to="/" className={`${classes.dropdown.link}`}>
-                          <LifebuoyIcon className={classes.dropdown.icon} />
-                          <span>Support</span>
-                        </Link>
-                        <Link to="/" className={`${classes.dropdown.link}`}>
-                          <ChatBubbleLeftEllipsisIcon
-                            className={classes.dropdown.icon}
-                          />
-                          <span>Support</span>
-                        </Link>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </li>
+          
+                
               <li className="relative group">
                 <Link to="/about" className={`${classes.navLink}`}>
                   <span>About</span>
@@ -256,6 +108,7 @@ function Navbar(props) {
                 </li>
               )}
             </ul>
+          
 
             {auth.user && (
               <Menu as="div" className="relative inline-block">
