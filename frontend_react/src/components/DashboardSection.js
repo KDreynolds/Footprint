0855be1563ffdf26314c52b5b6c245ae96afd994
KDReynolds <kylereynoldsdev@gmail.com>
2024-02-14 21:19:17 -0700

    dashboard changes

--- frontend_react/src/components/DashboardSection.js
+++ frontend_react/src/components/DashboardSection.js
@@ -1,13 +1,9 @@
 import React from "react";
 import Section from "./Section";
 import SectionHeader from "./SectionHeader";
-import DashboardItems from "./DashboardItems";
 import { Link } from "./../util/router";
 import { useAuth } from "./../util/auth";
 import { useDropzone } from 'react-dropzone';
-
-
-
 
 function DashboardSection(props) {
   const auth = useAuth();
@@ -35,24 +31,20 @@ function DashboardSection(props) {
           className="text-center"
         />
         <div className="flex flex-wrap">
-          <div className="p-4 w-full md:w-1/2">
-            <div className="rounded border border-gray-200">
-              <DashboardItems />
-            </div>
-          </div>
-          <div className="p-4 w-full md:w-1/2">
+          {/* Adjusted layout to focus on a single view */}
+          <div className="p-4 w-full">
             <div className="p-6 rounded border border-gray-200 prose prose-a:text-blue-600 max-w-none">
-            <div {...getRootProps()} className="dropzone">
-        <input {...getInputProps()} />
-        {
-          isDragActive ?
-            <p>Drop the files here ...</p> :
-            <p>Drag 'n' drop some files here, or click to select files</p>
-        }
+              <div {...getRootProps()} className="dropzone">
+                <input {...getInputProps()} />
+                {
+                  isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+                }
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </Section>
   );
--- frontend_react/src/pages/_app.js
+++ frontend_react/src/pages/_app.js
@@ -10,7 +10,7 @@ import DashboardPage from "./dashboard";
 import AuthPage from "./auth";
 import SettingsPage from "./settings";
 import LegalPage from "./legal";
-import { Switch, Route, Router } from "./../util/router";
+import { Switch, Route, Router, useLocation } from "./../util/router";
 import PurchasePage from "./purchase";
 import NotFoundPage from "./404";
 import Footer from "./../components/Footer";
@@ -18,6 +18,26 @@ import "./../util/analytics";
 import Chat from "./../components/Chat";
 import { AuthProvider } from "./../util/auth";
 import { QueryClientProvider } from "./../util/db";
+
+function ConditionalFooter() {
+  let location = useLocation();
+  
+  // Do not render the Footer on the dashboard page
+  if (location.pathname === '/dashboard') {
+    return null;
+  }
+
+  return (
+    <Footer
+      size="md"
+      bgColor="bg-white"
+      bgImage=""
+      bgImageOpacity={1}
+      textColor=""
+      sticky={true}
+    />
+  );
+}
 
 function App(props) {
   return (
@@ -30,36 +50,19 @@ function App(props) {
 
             <Switch>
               <Route exact path="/" component={IndexPage} />
-
               <Route exact path="/about" component={AboutPage} />
-
               <Route exact path="/faq" component={FaqPage} />
-
               <Route exact path="/contact" component={ContactPage} />
-
               <Route exact path="/pricing" component={PricingPage} />
-
               <Route exact path="/dashboard" component={DashboardPage} />
-
               <Route exact path="/auth/:type" component={AuthPage} />
-
               <Route exact path="/settings/:section" component={SettingsPage} />
-
               <Route exact path="/legal/:section" component={LegalPage} />
-
               <Route exact path="/purchase/:plan" component={PurchasePage} />
-
               <Route component={NotFoundPage} />
             </Switch>
 
-            <Footer
-              size="md"
-              bgColor="bg-white"
-              bgImage=""
-              bgImageOpacity={1}
-              textColor=""
-              sticky={true}
-            />
+            <ConditionalFooter />
           </>
         </Router>
       </AuthProvider>
